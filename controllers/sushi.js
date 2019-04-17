82f747d4825b116d67ca79a8e19c6d1f626ceb7f
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-04-17 15:04:53 -0600

    App Create Route and New FormDone

--- controllers/sushi.js
+++ controllers/sushi.js
@@ -2,6 +2,41 @@ const express = require('express');
 const router  = express.Router();
 const Sushi  = require('../models/sushi');
 
+// INDEX ROUTE
+router.get('/', (req, res) => {
+    Sushi.find({}, (err, allSushi)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log(allSushi);
+            res.render('index.ejs')
+        }
+    }
+    )
+});
+
+// CREATE ROUTE
+router.post('/', (req, res) => {
+    if(req.body.isRaw === 'on'){
+        req.body.isRaw = true;
+    }else{
+        req.body.isRaw = false
+    }
+    Sushi.create(req.body, (err, newSushi) => {
+        if(err){
+            console.log(err)
+        }else{
+            console.log(newSushi)
+            res.redirect('/sushi')
+        }
+    });
+});
+
+// NEW ROUTE
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
 
 
 
